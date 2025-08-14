d6a11cb108ffa301aa863175e27f15476aee825e
JC Bombardelli <jc.bombardelli@live.com>
2025-08-13 22:53:37 -0300

    chore: Update development configurations and dependencies

--- iac/jest.config.js
+++ iac/jest.config.js
@@ -1,7 +1,7 @@
 module.exports = {
   testEnvironment: 'node',
   roots: ['<rootDir>/test'],
-  testMatch: ['**/*.test.ts'],
+  testMatch: ['**/*.(test|spec).ts'],
   transform: {
     '^.+\\.tsx?$': 'ts-jest'
   }
